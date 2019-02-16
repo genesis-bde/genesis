131b470383ea4a168912728a40e2710f27667c19
Paul <venard.paul@gmail.com>
2019-02-16 23:29:17 +0000

    add/display social network image

--- screens/SocialNetworksScreen.js
+++ screens/SocialNetworksScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Image } from 'react-native';
 
 
 export default class SocialNetworksScreen extends React.Component {
@@ -7,7 +7,21 @@ export default class SocialNetworksScreen extends React.Component {
     render() {
         return (
             <ScrollView style={styles.container}>
-                <Text>Bienvenue sur la page des réseaux sociaux</Text>
+                <Image
+                    resizeMode="contain"
+                    style={styles.image}
+                    source={require('../assets/images/social/facebook.png')}
+                />
+                <Image
+                    resizeMode="contain"
+                    style={styles.image}
+                    source={require('../assets/images/social/snapchat.png')}
+                />
+                <Image
+                    resizeMode="contain"
+                    style={styles.image}
+                    source={require('../assets/images/social/instagram.png')}
+                />
             </ScrollView>
         );
     }
@@ -16,7 +30,15 @@ export default class SocialNetworksScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
         paddingTop: 15,
         backgroundColor: '#fff',
     },
+    image: {
+        flex: 1,
+        height: undefined,
+        width: undefined
+    }
 });
