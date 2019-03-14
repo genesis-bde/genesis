a3c013965797efb3bc8ea9a20244c0a9212787cf
Paul <venard.paul@gmail.com>
2019-03-14 12:37:30 +0000

    projects css

--- components/Project.js
+++ components/Project.js
@@ -7,25 +7,25 @@ export default class Project extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            activeSections: [],
+            activeProjects: [],
         };
     }
 
-    _renderSectionTitle = section => {
+    _renderSectionTitle = project => {
         return (
             <View style={styles.content}>
-                <View></View>
+                <View>{project.content}</View>
 
             </View>
         );
     };
 
-    _renderHeader = section => {
+    _renderHeader = project => {
         return (
             <View style={styles.header}>
-                <View><Text style={styles.headerText}>{section.title}</Text></View>
+                <View><Text style={styles.headerText}>{project.title}</Text></View>
                 <View>
-                    {this.state.activeSections === section ?
+                    {this.state.activeProjects === project ?
                         <Entypo name="chevron-thin-up" size={12} color="#c6c6c6"/> :
                         <Entypo name="chevron-thin-down" size={12} color="#c6c6c6"/>
                     }
@@ -34,27 +34,27 @@ export default class Project extends Component {
         );
     };
 
-    _renderContent = section => {
+    _renderContent = project => {
         return (
             <View style={styles.content}>
-                <Text>{section.content}</Text>
+                <Text>{project.content}</Text>
             </View>
         );
     };
 
-    _updateSections = activeSections => {
-        this.setState({activeSections});
+    _updateSections = activeProjects => {
+        this.setState({activeProjects});
     };
 
 
     render() {
         const section = this.props.sectionInfo;
         return (
-            <View>
-                <Text>{section.title}</Text>
+            <View styles={styles.section}>
+                <Text style={styles.title}>{section.title.toUpperCase()}</Text>
                 <Accordion
                     sections={section.projects}
-                    activeSections={this.state.activeSections}
+                    activeSections={this.state.activeProjects}
                     renderSectionTitle={this._renderSectionTitle}
                     renderHeader={this._renderHeader}
                     renderContent={this._renderContent}
@@ -67,8 +67,18 @@ export default class Project extends Component {
 
 
 const styles = StyleSheet.create({
+    section: {
+        marginVertical: 20,
+        marginHorizontal: 3
+    },
     header: {
         flexDirection: 'row',
         justifyContent: 'space-between'
+    },
+    title : {
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        textTransform: 'uppercase'
     }
 });
